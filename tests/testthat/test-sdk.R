089e579573024a884b72a828378c619327381967
Prince Fefar <pfefar1@gmail.com>
2022-12-15 15:16:22 -0800

    changes

--- tests/testthat/test-sdk.R
+++ tests/testthat/test-sdk.R
@@ -1,6 +1,16 @@
-# source("../../R/sdk.R", chdir = TRUE)
+source("../../R/sdk.R", chdir = TRUE)
 library(testthat)
 
-test_that("check", {
-  expect_equal(hello(), "Hello, world!")
-})
+new_valid_buyer <- function() {
+  api_key <- "buyer_key"
+  ip_addr <- "127.0.0.1"
+  buyer <- new_buyer(api_key, ip_addr)
+  return(buyer)
+}
+
+new_invalid_buyer <- function() {
+  api_key <- "invalid_buyer_key"
+  ip_addr <- "127.0.0.1"
+  buyer <- new_buyer(api_key, ip_addr)
+  return(buyer)
+}
