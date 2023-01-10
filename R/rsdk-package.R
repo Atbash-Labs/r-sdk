1a4c0eb10df21c871b43f762416199c228156663
Prince Fefar <pfefar1@gmail.com>
2023-01-10 12:42:33 -0800

    add init package missing file

---
+++ R/rsdk-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+#' @aliases rsdk
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom httr content headers status_code http_type GET POST
+#'   PATCH PUT DELETE
+## usethis namespace: end
+NULL
